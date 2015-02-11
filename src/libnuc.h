75cb92b99e3b44a84eb0953384c1071ff6a7a9a4
grenaud <gabriel.reno@gmail.com>
2015-02-12 00:15:45 +0100

    3pop

--- src/libnuc.h
+++ src/libnuc.h
@@ -61,10 +61,10 @@ inline long double qterm(int i,long double r,long double e,long double y){
 }
 
 // Binomial probability of sampling ancestral and derived reads given q term
-inline long double Pad_given_irey(int a,int d,int i,long double r,long double e,long double y){
+inline long double pad_given_irey(int a,int d,int i,long double r,long double e,long double y){
 #ifdef DEBUGL2
     //cout<<"qterm begin\t"<<i<<"\t"<<r<<"\t"<<e<<"\t"<<y<<endl;
-    cout<<"Pad_given_irey begin\t"<< a<<"\t"<<d<<"\t"<<i<<"\t"<<r<<"\t"<<e<<"\t"<<y<<endl;
+    cout<<"pad_given_irey begin\t"<< a<<"\t"<<d<<"\t"<<i<<"\t"<<r<<"\t"<<e<<"\t"<<y<<endl;
 #endif
 
 
@@ -72,7 +72,7 @@ inline long double Pad_given_irey(int a,int d,int i,long double r,long double e,
     long double toreturn = ( (long double)(nChoosek(a+d,d)) ) * (powl(qtermA,d))* (powl(1.0-qtermA,a) )  ; //defined in lib gab
 
 #ifdef DEBUGL2
-    cout<<"Pad_given_irey res\ta="<< a<<"\t"<<d<<"\t"<<i<<"\t"<<r<<"\t"<<e<<"\t"<<y<<endl;    
+    cout<<"pad_given_irey res\ta="<< a<<"\t"<<d<<"\t"<<i<<"\t"<<r<<"\t"<<e<<"\t"<<y<<endl;    
     cout<< (long double)(nChoosek(a+d,d)) <<"\t"<<(powl(qtermA,d)) <<"\t"<< (1.0-powl(qtermA,a)) <<endl;
     cout<<"Comb= "<<( (long double)(nChoosek(a+d,d)) )<<endl;
     cout<<qtermA<<endl;
@@ -116,21 +116,128 @@ inline long double Pgeno_given_ytau(int i,long double y,long double tau_C,long d
 }
 
 // Sum over each of the 3 types of genotypes for two-population method (no admixture)
-inline long double Pad_given_reytau(int a,int d,long double r,long double e,long double y,long double freqcont,long double tau_C,long double tau_A){
+inline long double pad_given_reytau(int a,int d,long double r,long double e,long double y,long double freqcont,long double tau_C,long double tau_A){
 
     double long sumResult=0.0;
     for(int i=0;i<=2;i++){
-        sumResult += Pad_given_irey(a,d,i,r,e,freqcont)*Pgeno_given_ytau(i,y,tau_C,tau_A);
+        sumResult += pad_given_irey(a,d,i,r,e,freqcont)*Pgeno_given_ytau(i,y,tau_C,tau_A);
     }
     // result <- sum(sapply(c(0,1,2),
     //                   function(i){})
     //            );
     return sumResult;
+}
 
+//Sum over each of the 3 types of genotypes for three-population method (incorporating admixture)
+inline long double pad_given_reytau_dadi_threeP(int a,int d,long double r,long double e,long double y,long double z,long double freqcont, vector< vector<long double> * > * dadiTable,long double numhumy,long double numhumz){
+    /* cout<<"pad_given_reytau_dadi_threeP1\t"<<a<<"\t"<<d<<endl; */
+
+    long double  ycoord     = roundl( y * numhumy );
+    long double  zcoord     = roundl( z * numhumz );
+    long double  finalcoord = ycoord*(numhumy+1) + zcoord ; //TODO check for 
+    double long sumResult=0.0;
+    for(int i=0;i<=2;i++){
+	/* cout<<"pad_given_reytau_dadi_threeP1\ts="<<dadiTable->size()<<"\ts="<<dadiTable->at(finalcoord)->size()<<"\t"<<ycoord<<"\t"<<zcoord<<"\t"<<finalcoord<<"\t"<<i<<endl; */
+	/* cout<<"P "<<pad_given_irey(a,d,i,r,e,freqcont)<<endl; */
+	/* cout<<"d "<<dadiTable->at(finalcoord)->at(i)<<endl; */
+        sumResult += pad_given_irey(a,d,i,r,e,freqcont)*dadiTable->at(finalcoord)->at(i);
+	//pad_given_irey(a,d,i,r,e,freqcont)*Pgeno_given_ytau(i,y,tau_C,tau_A);	             
+    }
+
+    /* cout<<"pad_given_reytau_dadi_threeP2\t"<< */
+    /* 	a<<"\t"<< */
+    /* 	d<<"\t"<< */
+    /* 	r<<"\t"<< */
+    /* 	e<<"\t"<< */
+    /* 	y<<"\t"<< */
+    /* 	z<<"\t"<< */
+    /* 	freqcont<<"\t"<< */
+    /* 	numhumy<<"\t"<< */
+    /* 	numhumz<<endl; */
+    /* cout<<sumResult<<endl; */
+    //    result <- sum(sapply(c(0,1,2),function(i){Pad_given_irey(a,d,i,r,e,freqcont)*DadiTable[finalcoord,(i+1)]}))
+    return sumResult ;
+}
+
+inline string ld2string(long double i){
+    stringstream s;
+    s << std::setprecision(10)<<i;
+    return s.str();
+}
+
+
+inline vector< vector<long double>  * > * getDadiTableThreeP(long double tau_C,long double tau_A,long double admixrate,long double admixtime,long double innerdriftY,long double innerdriftZ,long double nC,long double nB,long double nA){
+    string cmd = "python  Dadi_three_pop_admix.py -c "+ld2string(tau_C)+" -a "+ld2string(tau_A)+" -x "+ld2string(admixrate)+" -t "+ld2string(admixtime)+" -y "+ld2string(innerdriftY)+" -z "+ld2string(innerdriftZ)+" -m "+ld2string(nC)+" -n "+ld2string(nA)+" -b "+ld2string(nB)+" ";
+    cout<<cmd<<endl;
+    string values = runCmdAndCaptureSTDOUTandSTDERR(cmd);
+    
+    vector<string>       numberS   = allTokens(values,'\n');
+    vector<long double>  numbersLD;
+
+    for(unsigned int i=0;i<numberS.size();i++){
+
+	if(!numberS[i].empty()){//avoid empty lines
+	    long double numFound = destringify<long double>(numberS[i]) ;
+	    //cout<<"#"<<numberS[i]<<"#"<<numFound<<"#"<<endl;
+	    numbersLD.push_back( numFound );
+	}
+    }
+
+    /* for(unsigned int i=0;i<numbersLD.size();i++){ */
+    /* 	cout<<"#"<<(i+1)<<"#"<<numbersLD[i]<<"#"<<endl; */
+    /* } */
+
+    int    numberOfCols  = nA+1;
+    double numberOfRows  = double(numbersLD.size())/(double(numberOfCols));
+
+    if(floor(numberOfCols) != numberOfCols){
+	cerr<<"Internal error, the number of numberOfCols in the dadi output is not a multiple of nA + 1 = "<<(nA+1)<<" in command "<<cmd<<endl;
+    }
+    
+    //cout<<numberOfCols<<"\t"<<nA<<"\t"<<nB<<"\t"<<numberS.size()<<endl;
+    vector< vector<long double>  * >  * toReturn = new vector< vector<long double> * >( int(numberOfRows) );
+    
+     for(int i=0;i<int(numberOfRows);i++){ 
+	 toReturn->at(i) =  new vector<long double> ( numberOfCols );
+     } 
+     //cout<<"nr "<<numberOfRows<<" nc "<<numberOfCols<<endl;
+
+     int numbersLDi=0;
+
+     //copy to matrix
+     for(int c=0;c<int(numberOfCols);c++){ 	     
+	 for(int r=0;r<int(numberOfRows);r++){ 
+	     toReturn->at(r)->at(c) = numbersLD[numbersLDi++];
+
+	 }
+     }
+
+     //sum per col
+     for(int r=0;r<int(numberOfRows);r++){ 
+	 long double sumAllNum  = 0.0;
+
+	 for(int c=0;c<int(numberOfCols);c++){ 	     
+	     sumAllNum += toReturn->at(r)->at(c);
+	     //cout<<"toReturn["<<r<<","<<c<<"] "<<toReturn->at(r)->at(c)<<" = numbersLD["<<numbersLDi<<"]"<<" = "<<numbersLD[numbersLDi]<<endl;
+	 }
+
+	 for(int c=0;c<int(numberOfCols);c++){ 	     
+	     toReturn->at(r)->at(c) = toReturn->at(r)->at(c) / sumAllNum;
+	     //sumAllNum += toReturn->at(r)->at(c);
+	     //cout<<"toReturn["<<r<<","<<c<<"] "<<toReturn->at(r)->at(c)<<endl;
+	 }
+
+
+	 //cout<<endl;
+     }
+
+     
+
+    return toReturn;
 }
 
 //  Log final posterior for two populations
-long double LogFinalTwoP(vector<freqSite> * tableData,long double e,long double r,long double tau_C,long double tau_A,bool contequalanchor){
+inline long double LogFinalTwoP(vector<freqSite> * tableData,long double e,long double r,long double tau_C,long double tau_A,bool contequalanchor){
 //     print(c(e,r,tau_C,tau_A))
 
 	// Case where the anchor population is the same as the putative contaminant population (3rd column of data file)
@@ -140,9 +247,11 @@ long double LogFinalTwoP(vector<freqSite> * tableData,long double e,long double 
 
 	    //result <- sum(apply(table,1,function(x){
 	    //sumterm += log(Pad_given_reytau(x[1],x[2],r,e,x[3],x[3],tau_C,tau_A))*x[4]
-	    long double toaddToSum= log(Pad_given_reytau(tableData->at(indexSite).ancCount,
-					    tableData->at(indexSite).derCount,
-					    r,e,tableData->at(indexSite).panelFreqCont,tableData->at(indexSite).panelFreqCont,tau_C,tau_A))*tableData->at(indexSite).num;
+	    long double toaddToSum = log(pad_given_reytau(tableData->at(indexSite).ancCount,
+							  tableData->at(indexSite).derCount,
+							  r,e,
+							  tableData->at(indexSite).panelFreqCont,
+							  tableData->at(indexSite).panelFreqCont,tau_C,tau_A))*tableData->at(indexSite).num;
 	    //cout<<tableData->at(indexSite).ancCount<<"\t"<<tableData->at(indexSite).derCount<<"\t"<<tableData->at(indexSite).panelFreqCont<<"\t"<<tableData->at(indexSite).num<<"\t"<<toaddToSum<<endl;
 	    sumterm+=toaddToSum;
 	}
@@ -165,47 +274,90 @@ long double LogFinalTwoP(vector<freqSite> * tableData,long double e,long double 
 //     if(is.na(result)){return(-1000000000000000)}
 //     else{return(result)}
 }
-
 
 
 
 //  Log final posterior for three populations
-long double LogFinalThreeP(vector<freqSite> * tableData,long double e,long double r,long double tau_C,long double tau_A,long double admixrate,long double admixtime,long double innerdriftY,long double innerdriftZ,long double nC,long double nB,bool contequalanchor){
-//     print(c(e,r,tau_C,tau_A))
+inline long double LogFinalThreeP(vector<freqSite> * tableData,long double e,long double r,long double tau_C,long double tau_A,long double admixrate,long double admixtime,long double innerdriftY,long double innerdriftZ,long double nC,long double nB,bool contequalanchor){
+    //     print(c(e,r,tau_C,tau_A))
 
-	// Case where the anchor population is the same as the putative contaminant population (3rd column of data file)
-    if(contequalanchor){
-        long double sumterm=0.0;
-        for(unsigned int indexSite=0;indexSite<tableData->size();indexSite++){
-
-	    //result <- sum(apply(table,1,function(x){
-	    //sumterm += log(Pad_given_reytau(x[1],x[2],r,e,x[3],x[3],tau_C,tau_A))*x[4]
-	    long double toaddToSum= log(Pad_given_reytau(tableData->at(indexSite).ancCount,
-					    tableData->at(indexSite).derCount,
-					    r,e,tableData->at(indexSite).panelFreqCont,tableData->at(indexSite).panelFreqCont,tau_C,tau_A))*tableData->at(indexSite).num;
-	    //cout<<tableData->at(indexSite).ancCount<<"\t"<<tableData->at(indexSite).derCount<<"\t"<<tableData->at(indexSite).panelFreqCont<<"\t"<<tableData->at(indexSite).num<<"\t"<<toaddToSum<<endl;
-	    sumterm+=toaddToSum;
-	}
-	return sumterm;
-	// }))
+    if( (e < 0)     || 
+	(r < 0)     || 
+	(tau_C < 0) || 
+	(tau_A < 0) || 
+	(admixrate < 0) || admixtime < 0){
+        return(-1000000000000000);
     }else{
-	// Case where the anchor population is different from the putative contaminant population
-	// Anchor population frequencies are in third column of data file
-	//Putative contaminant population frequencies are in fourth columnn of data file
-	//TODO
-        //          result <- sum(apply(table,1,function(x){
-        //          sumterm <- log(Pad_given_reytau(x[1],x[2],r,e,x[3],x[4],tau_C,tau_A))*x[5]
-        //                   return(sumterm)
-        // }))
-	return 0;
+        long double nA = 2.0;
+	vector< vector<long double> * > * dadiTable= getDadiTableThreeP(tau_C,
+								    tau_A,
+								    admixrate,
+								    admixtime,
+								    innerdriftY,
+								    innerdriftZ,
+								    nC,
+								    nB,
+								    nA);
+	//cout<<"after getDadiTableThreeP"<<endl;
+	long double sumterm=0.0;
+	if(contequalanchor){
+
+
+	    for(unsigned int indexSite=0;indexSite<tableData->size();indexSite++){
+		//cout<<"LogFinalThreeP1 "<<indexSite<<endl;
+		long double toaddToSum=0;
+		toaddToSum = log(pad_given_reytau_dadi_threeP(tableData->at(indexSite).ancCount,
+							      tableData->at(indexSite).derCount,
+							      r,
+							      e,
+							      tableData->at(indexSite).panelFreqCont,
+							      tableData->at(indexSite).panelFreqAdmx ,
+							      tableData->at(indexSite).panelFreqCont,
+							      dadiTable,
+							      nC,
+							      nB)
+					     ) * tableData->at(indexSite).num;
+		//cout<<"LogFinalThreeP2 "<<indexSite<<endl;
+		sumterm+=toaddToSum;
+	    }
+	    //cout<<"LogFinalThreeP done "<<endl;
 	
+	    
+            /* result <- sum(apply(table,1,function(x){ */
+            /*     sumterm <- log(Pad_given_reytau_dadi_threeP(x[1],x[2],r,e,x[3],x[4],x[3],DadiTable,nC,nB))*x[5] */
+            /*     return(sumterm) */
+            /* })) */
+        }
+
+
+	// Case where anchor populations are different from the putative contaminant population
+	// Frequencies of anchor population 1 would be in the third column of data file
+        // Frequencies of anchor population 2 would be in the fourth column of data file
+        // Contaminant population frequencies would be in the fifth column of data file        
+        else{
+	    //TODO
+	    exit(1);
+	}
+
+
+	//destroy table	
+	for(unsigned int i=0;i<dadiTable->size();i++){
+	
+	    /* for(unsigned int j=0;j<dadiTable->at(i)->size();j++){ */
+	    /* 	delete dadiTable->at(i)->at(j); */
+	    /* 	//     cout<<dadiTable->at(i)->at(j).x <<","<<dadiTable->at(i)->at(j).y <<"\t"; */
+	    /* } */
+	    // cout<<endl;
+	    delete dadiTable->at(i);
+	}
+	
+	delete dadiTable;
+	return sumterm;
+
+
     }
 
-//     print(result)
-//     if(is.na(result)){return(-1000000000000000)}
-//     else{return(result)}
 }
-
 
 
 /* class libnuc{ */
--- src/nucularMCMC.cpp
+++ src/nucularMCMC.cpp
@@ -26,20 +26,46 @@ int main (int argc, char *argv[]) {
    myFile.open(filename.c_str(), ios::in);
    vector<freqSite> * dataToAdd =  new vector<freqSite>();
 
+
+   bool firstIteration  = true;
+   bool twoPopMode  = false;
+   bool threePopMode = false;
+
    if (myFile.good()){
        getline (myFile,line);//header
        while ( getline (myFile,line)){
 	   vector<string> fields = allTokens(line,'\t');
-	   if(fields.size() != 4){
-	       cerr<<"Line "<<line<<" does not contain 4 fields"<<endl;
+	   if(fields.size() != 4 &&
+	      fields.size() != 5){
+	       cerr<<"Line "<<line<<" does not contain 4 or 5 fields but "<<fields.size()<<" fields"<<endl;
 	       exit(1);
+	   }
+	   
+	   if(firstIteration){
+	       twoPopMode   =  ( fields.size() == 4 );
+	       threePopMode =  ( fields.size() == 5 );
+	       firstIteration=false;
 	   }
 
 	   freqSite toaddF;
-	   toaddF.ancCount      = destringify<int>         (fields[0]);
-	   toaddF.derCount      = destringify<int>         (fields[1]);
-	   toaddF.panelFreqCont = destringify<long double >(fields[2]);
-	   toaddF.num           = destringify<int>         (fields[3]);
+
+	   if(twoPopMode){
+	       toaddF.ancCount      = destringify<int>         (fields[0]);
+	       toaddF.derCount      = destringify<int>         (fields[1]);
+	       toaddF.panelFreqCont = destringify<long double >(fields[2]);
+	       toaddF.num           = destringify<int>         (fields[3]);
+	   }else{
+	       if(threePopMode){
+		   toaddF.ancCount      = destringify<int>         (fields[0]);
+		   toaddF.derCount      = destringify<int>         (fields[1]);
+		   toaddF.panelFreqCont = destringify<long double >(fields[2]);
+		   toaddF.panelFreqAdmx = destringify<long double >(fields[3]);
+		   toaddF.num           = destringify<int>         (fields[4]);
+	       }else{
+		   cerr<<"Line "<<line<<" does not contain 4 or 5 fields"<<endl;
+		   exit(1);
+	       }
+	   }
 	   dataToAdd->push_back(toaddF);
 
        }
@@ -48,51 +74,79 @@ int main (int argc, char *argv[]) {
        cerr << "Unable to open file "<<filename<<endl;
        return 1;
    }
+   cerr<<"done"<<endl;
+
+   if( twoPopMode  == threePopMode ){
+       cerr << "Internal error, cannot have two and three pops at once"<<endl;
+       return 1;
+   }
 
    // Set lower boundaries for optimization algorithm
-   long double elower  = 0.00001;
-   long double rlower  = 0.00001;
-   long double tau_Clower  = 0.000001;
-   long double tau_Alower  = 0.000001;
+   long double elower         = 0.00001;
+   long double rlower         = 0.00001;
+   long double tau_Clower     = 0.000001;
+   long double tau_Alower     = 0.000001;
+   long double admixratelower = 0.000001;
+   long double admixtimelower = 0.05;
 
    // Set upper boundaries for optimization algorithm
-   long double eupper  = 0.1;
-   long double rupper  = 0.5;
-   long double tau_Cupper  = 1.0;
-   long double tau_Aupper  = 1.0;
+   long double eupper         = 0.1;
+   long double rupper         = 0.5;
+   long double tau_Cupper     = 1.0;
+   long double tau_Aupper     = 1.0;
+   long double admixrateupper = 0.5;
+   long double admixtimeupper = 0.11;
 
-   long double e_i     = randomLongDouble(elower,eupper);
-   long double r_i     = randomLongDouble(rlower,rupper);
-   long double tau_C_i = randomLongDouble(tau_Clower,tau_Cupper);
-   long double tau_A_i = randomLongDouble(tau_Alower,tau_Aupper);
 
-   // long double e_i     = 0.002; 
-   // long double r_i     = 0.038809028;
-   // long double tau_C_i = 0.531137046;
-   // long double tau_A_i = 0.560341912;
-   // long double e_i     = 0.001105315; 
-   // long double r_i     = 0.038809028;
-   // long double tau_C_i = 0.531137046;
-   // long double tau_A_i = 0.560341912;
+   //Constants
+   long double innerdriftY =   0.16;
+   long double innerdriftZ =   0.16;
+   long double nC          =  20.0 ;
+   long double nB          =  20.0 ;
 
+   //Vector Variables
+   long double e_i         = randomLongDouble(elower,         eupper);
+   long double r_i         = randomLongDouble(rlower,         rupper);
+   long double tau_C_i     = randomLongDouble(tau_Clower,     tau_Cupper);
+   long double tau_A_i     = randomLongDouble(tau_Alower,     tau_Aupper);
+   long double admixrate_i = randomLongDouble(admixratelower, admixrateupper);
+   long double admixtime_i = randomLongDouble(admixtimelower, admixtimeupper);
 
 
    long double e_i_1;
    long double r_i_1;
    long double tau_C_i_1;
    long double tau_A_i_1;
+   long double admixrate_i_1;
+   long double admixtime_i_1;
+
+   //to test
+
+   // long double e          = 0.06959408;
+   // long double r          = 0.31454792;
+   // long double tau_C      = 0.34873276; 
+   // long double tau_A      = 0.02521097; 
+   // long double admixrate  = 0.16149144; 
+   // long double admixtime  = 0.10068321;
+
+   // cout<<LogFinalThreeP(dataToAdd,e,r,tau_C,tau_A,admixrate,admixtime,innerdriftY,innerdriftZ,nC,nB,true)<<endl;
+   // cout<<"done "<<endl;
+   // return 1;
 
 
-   // long double e     = 0.02228953;
-   // long double r     = 0.33323844;
-   // long double tau_C = 0.41876066;
-   // long double tau_A = 0.71627768;
-   long double x_il    = LogFinalTwoP(dataToAdd,e_i,r_i,tau_C_i,tau_A_i,true);
+   long double x_il;
    long double x_i_1l;
 
+   if(twoPopMode){
+       x_il = LogFinalTwoP(  dataToAdd,e_i,r_i,tau_C_i,tau_A_i,true);
+   }else{
+       x_il = LogFinalThreeP(dataToAdd,e_i,r_i,tau_C_i,tau_A_i,admixrate_i,admixtime_i,innerdriftY,innerdriftZ,nC,nB,true);
+   }
+
    for(unsigned int chain=0;chain<100000;chain++){
-       cout<<"current "<<std::setprecision(10)<<x_il<<"\t"<<e_i<<"\t"<<r_i<<"\t"<<tau_C_i<<"\t"<<tau_A_i<<endl;
+       cout<<"current "<<std::setprecision(10)<<x_il<<"\t"<<e_i<<"\t"<<r_i<<"\t"<<tau_C_i<<"\t"<<tau_A_i<<"\t"<<admixrate_i<<"\t"<<admixtime_i<<endl;
        long double partition=1000.0;
+
        //e
        long double facte = fmod((long double)(randomProb()), (eupper-elower)/partition );
        cout<<facte<<endl;
@@ -126,19 +180,45 @@ int main (int argc, char *argv[]) {
             tau_A_i_1=tau_A_i-facttau_A;
         }
 
-       x_i_1l    = LogFinalTwoP(dataToAdd,e_i_1,r_i_1,tau_C_i_1,tau_A_i_1,true);
+       if(!twoPopMode){
+
+	   //admix_rate  
+	   long double factadmixrate = fmod( (long double)(randomProb()), (admixrateupper-admixratelower)/partition);
+	   if(randomBool()){
+	       admixrate_i_1=admixrate_i+factadmixrate;
+	   }else{
+	       admixrate_i_1=admixrate_i-factadmixrate;
+	   }
+
+	   //admix_time  
+	   long double factadmixtime = fmod( (long double)(randomProb()), (admixtimeupper-admixtimelower)/partition);
+	   if(randomBool()){
+	       admixtime_i_1=admixtime_i+factadmixtime;
+	   }else{
+	       admixtime_i_1=admixtime_i-factadmixtime;
+	   }
+       }
+
+       if(twoPopMode){
+	   x_i_1l    = LogFinalTwoP(  dataToAdd,e_i_1,r_i_1,tau_C_i_1,tau_A_i_1,                                                          true);
+       }else{
+	   x_i_1l    = LogFinalThreeP(dataToAdd,e_i_1,r_i_1,tau_C_i_1,tau_A_i_1,admixrate_i_1,admixtime_i_1,innerdriftY,innerdriftZ,nC,nB,true);
+       }
+
        long double acceptance = min( (long double)(1.0)  , expl(x_i_1l-x_il) );
-       cout<< "new "<<std::setprecision(10)<<x_i_1l<<"\t"<<e_i_1<<"\t"<<r_i_1<<"\t"<<tau_C_i_1<<"\t"<<tau_A_i_1<<"\t"<<acceptance<<endl;
 
-
+       cout<< "new   "<<std::setprecision(10)<<x_i_1l<<"\t"<<e_i_1<<"\t"<<r_i_1<<"\t"<<tau_C_i_1<<"\t"<<tau_A_i_1<<"\t"<<admixrate_i_1<<"\t"<<admixtime_i_1<<"\t"<<acceptance<<endl;
        cout<< "ratio "<<std::setprecision(10)<<expl(x_i_1l-x_il)<<"\t"<<(x_i_1l-x_il)<<endl;
 
-       if((long double)(randomProb()) < acceptance){
-	   e_i     =  e_i_1;
-	   r_i     =  r_i_1;
-	   tau_C_i =  tau_C_i_1;
-	   tau_A_i =  tau_A_i_1;	  
+       if( (long double)(randomProb()) < acceptance){
+	   e_i           =  e_i_1;
+	   r_i           =  r_i_1;
+	   tau_C_i       =  tau_C_i_1;
+	   tau_A_i       =  tau_A_i_1;	  
+	   admixrate_i   =  admixrate_i_1;
+	   admixtime_i   =  admixtime_i_1;
 	   x_il      = x_i_1l;
+
 	   cout<<"new state"<<endl;
        }else{
 	   cout<<"reject"<<endl;
